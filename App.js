dd122b84513d54a021b4e05f3c0d7dd4ce1bdd5d
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-07-18 15:22:41 +0530

    use React Components

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { Component } from "react"
 import ReactDOM from "react-dom/client"
 
 // React.createElement =>ReactElement -JS Object => HTMLElement(render)
@@ -6,18 +6,27 @@ import ReactDOM from "react-dom/client"
 const heading = React.createElement("h1",{id:"heading"},"Namaste React");
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-const root1 = ReactDOM.createRoot(document.getElementById("root1"))
 
-root.render(heading)
 
-console.log(heading);
-
-// JSX (transpiled before it reaches the JS Engine) =>Converted , by Parcel  - Babel helps Parcel... it converted the code quickly to a code that reactt will understand
-
-// JSX => React.createElement =>ReactElement -JS Object => HTMLElement(render)
 
 const jsxHeading= <h1 id="heading1">Namaste React using JSX</h1>
 
-root1.render(jsxHeading)
 
-console.log(jsxHeading);
+// Component Composition -->
+const HeadingComponent = () => <div id="container">
+    {heading}
+    {jsxHeading}
+    <TitileComponents/>
+    <h1 id="heading">Namaste React Functional Components</h1>
+    
+</div>
+
+const TitileComponents=()=> <div>
+    <h1 id="heading1"> Hii I'm Title Component</h1>
+</div>
+
+
+root.render(<>
+<TitileComponents/>
+<HeadingComponent/>
+</>)
