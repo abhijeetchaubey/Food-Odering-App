9bd5cb09980f580ce5f28a497b2f558d40ea5d93
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-07-20 17:58:34 +0530

    update body component

---
+++ src/components/Body.js
@@ -0,0 +1,43 @@
+
+import RestraurantCard from "./RestraurantCard"
+import resList from "../utils/mockData"
+
+import { useState } from "react";
+
+
+const Body =()=>{
+
+    // Local State Variable -Super Powefull Variable
+
+    const [listofRestaurant,setlistofRestaurant]=useState(resList);
+
+    return(
+        <div className="body">
+            <div className="filter">
+                <button
+                className="filter-btn"
+                title="Top rated Restraurant"
+                onClick={()=>{
+                    let filteredList=listofRestaurant.filter(
+                        (res)=> res?.info?.avgRating >=4
+                    );
+                    console.log(filteredList);
+                    setlistofRestaurant(filteredList)
+                }}
+                >
+                    Top Rated Restaurant</button>
+            </div>
+            <div className="restraunt-container">
+                {
+                    listofRestaurant.map(restraunt=>(
+                    <RestraurantCard key={restraunt.info.id} resList={restraunt} />))
+                }
+                
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Body
