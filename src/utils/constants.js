533e401a8e9cbd49d4748229231124c76b55ee95
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-07-22 23:30:08 +0530

    update Constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const CDN_URL ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
-export const BURGER_KING_IMG='https://i.insider.com/5bd2234bdde867488579161f?width=700'
+export const BURGER_KING_IMG='RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/3b777c8a-b1fe-4df4-86cf-67c6bf9a0886_240107.jpg'
 
 export const LOGO_URL="https://marketplace.canva.com/EAFaFUz4aKo/2/0/1600w/canva-yellow-abstract-cooking-fire-free-logo-JmYWTjUsE-Q.jpg"
 
