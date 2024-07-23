d0ebf845f76d9b48f6de63f47b8910e7962554ff
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-07-23 21:50:13 +0530

    update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { LOGO_URL } from "../utils/constants"
+import { Link, NavLink } from "react-router-dom";
 
 const Header =()=>{
     const[btnName,setbtnName]=useState("Login");
+    console.log("header render");
+
+    // if no depencency array => useEffect is called on every render
+    // if dependency array is empty=[]=> useEffect is called on initial render (just once) 
+    // if dependency array is [btnName] => called every time btnName is updated
+    // useEffect(()=>{
+    //     console.log("useEffect called");
+    // },[btnName]);
+
     return(
         <div className="header">
             <div className="logo-container">
@@ -11,9 +21,15 @@ const Header =()=>{
             <div className="nav-items">
                 <div>
                     <ul>
-                        <li>Home</li>
-                        <li>About Us</li>
-                        <li>Contact Us</li>
+                        <li>
+                        <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                        <NavLink to="/about">About US</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact">Contact Us</NavLink>
+                        </li>
                         <li>Cart</li>
                         <button
                         className="login"
