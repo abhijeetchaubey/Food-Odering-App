8d70fedd84f45a520f4da566131b4a11512f7f42
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-07-23 21:48:53 +0530

    imported react router dom

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import Body from "./components/Body"
-
-
+import { createBrowserRouter,Outlet,RouterProvider } from "react-router-dom"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import Error from "./components/Error"
+import RestraurantMenu from "./components/RestraurantMenu"
 
 
 // Here Props is an object
@@ -17,7 +20,7 @@ const AppLayout =()=>{
     return(
         <div className="app">
             <Header/>
-            <Body/>
+            <Outlet/>
             {/* 
                 Footer
             */}
@@ -25,6 +28,42 @@ const AppLayout =()=>{
     )
 }
 
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element:<AppLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<Body/>
+            },
+            {
+                path:"/about",
+                element:<About/>
+            },
+            {
+                path:"/contact",
+                element:<Contact/>
+            }
+        ],
+        errorElement:<Error/>
+    },
+    {
+        path:"/about",
+        element:<About/>
+    },
+    {
+        path:"/contact",
+        element:<Contact/>
+
+    },
+
+    {
+        path:"/restraurants/:resId",
+        element:<RestraurantMenu/>
+    }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<AppLayout/>)
+root.render(<RouterProvider router={appRouter} />)
