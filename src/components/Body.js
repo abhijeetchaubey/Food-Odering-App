d0e46a06cd25ab283bb53b00da50c02d07d1b0c7
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-07-24 22:17:39 +0530

    update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,23 +2,24 @@
 import RestraurantCard from "./RestraurantCard"
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer"
+import { Link } from "react-router-dom";
+
 
 const Body =()=>{
 
     // Local State Variable -Super Powefull Variable
     const [listofRestaurant,setlistofRestaurant]=useState([]);
     const [filteredRestraurant,setfilteredRestaurant]=useState([])
-
     const [searchText,setsearchText]=useState("")
 
     // console.log("Body Rendered"); 
 
     useEffect(()=>{
         fetchData();
-        // fetchUpdate()
 
     },[])
 
+    // swiggy api for restaurant
     const fetchData=async()=>{
         const data = await fetch(
             "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.3164945&lng=78.03219179999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
@@ -30,40 +31,6 @@ const Body =()=>{
         setlistofRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
         setfilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     };
-
-    const fetchUpdate=async()=>{
-        const url = "https://www.swiggy.com/dapi/restaurants/list/update"
-        
-        const headers={
-            "Content-Type": "application/json"
-        };
-        const data={
-            "lat":"30.3164945",
-            "lang":"78.03219179999999"
-        };
-        try {
-             // Make the POST request
-        const response = await fetch(url, {
-            method: "POST",
-            headers: headers,
-            body: JSON.stringify(data)
-        });
-        
-        // Check if the request was successful
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        
-        // Parse the JSON response
-        const json = await response.json();
-        console.log("Response JSON:", json);
-        
-        // Handle the JSON response here
-        return json;
-        } catch (error) {
-            console.error("Error fetching data:",error)
-        }
-    }
 
 
     return listofRestaurant.length=== 0 ? <Shimmer/> :(
@@ -93,25 +60,31 @@ const Body =()=>{
                 <button
                 className="filter-btn"
                 title="Top rated Restraurant"
+                // .data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.avgRating
                 onClick={()=>{
                     let filteredList=listofRestaurant.filter(
                         (res)=> res?.info?.avgRating >=4
                     );
                     console.log(filteredList);
-                    setlistofRestaurant(filteredList)
+                    setfilteredRestaurant(filteredList)
                 }}
-                >Top Rated Restaurant</button>
+                >Top Rated Restaurant</button> 
             </div>
+            
             <div className="restraunt-container">
-                {
-                    filteredRestraurant.map(restraunt=>(
-                    <RestraurantCard key={restraunt.info.id} resList={restraunt} />))
-                }
-                
+                {filteredRestraurant.map((restraunt)=>(
+                    <Link
+                    to={"/restraurants/"+restraunt.info.id}
+                    key={restraunt.info.id}
+                    style={{ textDecoration: 'none', color: 'inherit'}}
 
-
+                    >
+                        <RestraurantCard resList={restraunt} />
+                    </Link>
+                ))}
             </div>
         </div>
+        
     )
 }
 
