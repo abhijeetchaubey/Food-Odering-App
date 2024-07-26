91ad650493e477a05e674040079067606e8e9fd3
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-07-26 19:21:21 +0530

    Added Lazy loading and seprate the bundlers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { lazy,Suspense } from "react"
 import ReactDOM from "react-dom/client"
 import Header from "./components/Header"
 import Body from "./components/Body"
@@ -7,14 +7,16 @@ import About from "./components/About"
 import Contact from "./components/Contact"
 import Error from "./components/Error"
 import RestraurantMenu from "./components/RestraurantMenu"
+// import Grocery from "./components/Grocery"
 
 
-// Here Props is an object
+// Chunking
+// Code Splitting
+// Dynamic Bundling
+// Lazy Loading
+// on demand loading
 
-
-
-
-
+const Grocery =lazy(()=>import("./components/Grocery"))
 
 const AppLayout =()=>{ 
     return(
@@ -48,7 +50,12 @@ const appRouter = createBrowserRouter([
             {
                 path:"/restraurants/:resId",
                 element:<RestraurantMenu/>
+            },
+            {
+                path:"/grocery",
+                element:<Suspense fallback={<h1>Loading....</h1>} Grocery/>
             }
+            
         ],
         errorElement:<Error/>
     },
