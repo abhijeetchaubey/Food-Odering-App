67ba37999324d86e7eee4428e2a4865d6067c2c2
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-07-26 19:22:35 +0530

    Add functionality to check online status

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,7 @@ import RestraurantCard from "./RestraurantCard"
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer"
 import { Link } from "react-router-dom";
-
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body =()=>{
 
@@ -32,6 +32,10 @@ const Body =()=>{
         setfilteredRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     };
 
+
+    const onlineStatus = useOnlineStatus()
+
+    if(!onlineStatus) return <h1>Looks like you are offline!! check your internet connection</h1>
 
     return listofRestaurant.length=== 0 ? <Shimmer/> :(
         <div className="body">
