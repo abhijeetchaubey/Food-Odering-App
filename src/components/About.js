c181ffde11395412774fc80a0291d531a999db6d
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-07-26 19:22:14 +0530

    create class based Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,41 @@
-const About=()=>{
-    return (
-        <div>
-            <h1>About</h1>
-            <h3>This is Food Odering Website</h3>
-        </div>
-    );
-};
+// import User from "./User"
+import UserClass from "./UserClass";
+import React from "react";
 
-export default About;
+class About extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    componentDidMount(){
+        console.log("Parent Component Did Mount");
+    }
+
+    render(){
+
+        console.log("Parent render");
+        return (
+            <div>
+                <h1>About</h1>
+                <h3>This is Food Odering Website</h3>
+
+                <UserClass name={"Abhijeet Chaubey"} location={"Dehradun"}/>
+            </div>
+        )
+    }
+}
+
+// const About=()=>{
+//     return (
+//         <div>
+//             <h1>About</h1>
+//             <h3>This is Food Odering Website</h3>
+//             {/* <User name={"Abhijeet Chaubey"}/>
+//  */}
+
+//             <UserClass name={"Abhijeet Chaubey (class"} location={"Dehradun"}/>
+//         </div>
+//     )
+// };
+
+export default About; 
---
+++ src/components/UserClass.js
@@ -0,0 +1,63 @@
+// Class based Component used in Older React 
+import React from "react"
+
+
+class UserClass extends React.Component {
+    constructor(props){
+        super(props); // Call the parent class constructor with props
+
+        this.state={
+        userInfo:{
+            name:"Dummy",
+            location:"Default"
+        }
+        }
+    }
+
+    async componentDidMount(){
+        // call Api here
+        const data = await fetch("https://api.github.com/users/abhijeetchaubey")
+        const json = await data.json()
+
+        console.log(json);
+        this.setState({
+            userInfo:json,
+
+        })
+    }
+
+    componentDidUpdate(){
+        console.log("Component Did Update");
+    }
+
+    componentWillUnmount(){
+        
+    }
+    render(){
+        const {name,location,avatar_url}=this.state.userInfo
+        return( 
+            <div className="user-card">
+                <img src={avatar_url} />
+                <h2>Name:{name}</h2>
+                <h3>Location:{location}</h3>
+                <h4>Contact:@abhijeet15</h4>
+            </div>
+        )
+    }
+}
+
+export default UserClass;
+
+//***Component Life Cycle***
+// Constructor(dummy)
+// Render (dummy)
+//      <HTML Dummy>
+// Component Did Mount
+//      <API Call>
+//      <this.states> ->State variable is updated
+
+// ------UPDATE
+
+//   render(API data)
+//   <HTML (new API data)>
+//      componentDid Update
