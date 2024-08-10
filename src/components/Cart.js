9cc3367526915dadaec20a495cfb3563f7cf4297
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-08-10 23:08:18 +0530

    add tailwind css to make carts responsive

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
-import { clearCart } from "../utils/cartslice";
-
+import { clearCart,removeItem } from "../utils/cartslice";
+import { EMPTY_CART_IMG } from "../utils/constants";
+import { Link } from "react-router-dom";
 const Cart =()=>{
 
     const cartItems = useSelector((store=>store.cart.items))
@@ -11,18 +12,35 @@ const Cart =()=>{
     const handleClearClick=()=>{
         dispatch(clearCart())
     }
+    const handleRemoveCart=()=>{
+        dispatch(removeItem())
+    }
     
     return(
         <div className=" m-6 p-4">
-            <h1 className="text-2xl text-center font-bold">Cart</h1>
             <div className="w-6/12 m-auto">
             <button 
             onClick={handleClearClick}
             className={`rounded-md bg-black text-white ${cartItems.length > 0 ? 'p-1 w-[100px] ml-[90%]' : ''}`}>
             {cartItems.length > 0 ? "Clear Cart" : null}
     </button>
-                {cartItems.length===0 && <h1 className="font-semibold text-2xl">Cart is Empty. Add Items to your Cart</h1>}
+    <button
+    onClick={handleRemoveCart}
+    className={`rounded-md bg-black text-white ${cartItems.length > 0 ? 'p-1 w-[100px] ml-[40%]' : ''}`}>
+    {cartItems.length > 0 ? "Remove Item" : null}
+    </button>
+                {cartItems.length===0 && <img 
+                className="ml-[30%] w-[300px] h-[250px] mt-20"
+                src={EMPTY_CART_IMG}/>}
+                {cartItems.length===0 && <h1 className="font-semibold text-xl text-slate-800 text-center mt-5">Your cart is empty</h1>}
+                
+                {cartItems.length===0 && <p className="font-thin text-md text-slate-500 text-center mt-1">You can go to home page to view more restraurants</p>}
                 <ItemList items={cartItems}/>
+                <div className="mt-10 text-center">
+                <Link 
+                className={` text-white font-semibold ${cartItems.length >0 ? '':'bg-orange-500  min-w-fit p-2'}`}
+                to={"/"}>SEE RESTRAURANT NEAR YOU</Link>
+                </div>
             </div>
         </div>
     )
