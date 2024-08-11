a2a7c27e39110a8be93ae54895ae3108808fc96a
Abhijeet Chaubey <chaubeyabhijeet36@gmail.com>
2024-08-11 21:34:28 +0530

    Change CSS

--- src/components/About.js
+++ src/components/About.js
@@ -1,44 +1,28 @@
-// import User from "./User"
 import UserContext from "../utils/UserContext";
 import UserClass from "./UserClass";
 import React from "react";
 
-
-class About extends React.Component{
-    constructor(props){
-        super(props)
+class About extends React.Component {
+    constructor(props) {
+        super(props);
     }
 
-    componentDidMount(){
+    componentDidMount() {
         console.log("Parent Component Did Mount");
     }
 
-    render(){
-
+    render() {
         console.log("Parent render");
         return (
-            <div>
-                <h1>About</h1>
-                
-                <h3>This is Food Odering Website</h3>
-
-                <UserClass name={"Abhijeet Chaubey"} location={"Dehradun"}/>
+            <div className="container mx-auto px-4 py-8">
+                <h1 className="text-3xl font-bold text-center mb-4">About</h1>
+                <h3 className="text-xl text-center mb-6">This is a Food Ordering Website</h3>
+                <div className="flex justify-center">
+                    <UserClass name={"Abhijeet Chaubey"} location={"Dehradun"} />
+                </div>
             </div>
-        )
+        );
     }
 }
 
-// const About=()=>{
-//     return (
-//         <div>
-//             <h1>About</h1>
-//             <h3>This is Food Odering Website</h3>
-//             {/* <User name={"Abhijeet Chaubey"}/>
-//  */}
-
-//             <UserClass name={"Abhijeet Chaubey (class"} location={"Dehradun"}/>
-//         </div>
-//     )
-// };
-
-export default About; 
+export default About;
